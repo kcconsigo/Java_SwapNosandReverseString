a95a030beb80b2563d125461ad1937047f8d6cad
Kc Consigo <kcconsigo@gmail.com>
2023-06-12 19:25:36 +0800

    Update SwapNos code

--- src/SwapNos.java
+++ src/SwapNos.java
@@ -16,19 +16,19 @@ public class SwapNos {
 		
 		System.out.println("values swap after: x = " + x +" y = " + y);
 		
-		/*
-		 * System.out.println("Calling swap function with inputs 2 & 3"); swap(2,3);
-		 * System.out.println("Calling swap function with inputs 2 & 3"); swap(-3,5);
-		 * 
-		 * }
-		 * 
-		 * private static void swap(int i, int j) { // TODO Auto-generated method stub
-		 * 
-		 * System.out.println("values swap before: " + i +" + " +j); //swap logic i = i
-		 * + j; j = i - j; i = i - j;
-		 * 
-		 * System.out.println("values swap after: " + i +" + " +j);
-		 */
+		
+		  System.out.println("Calling swap function with inputs 2 & 3"); swap(2,3);
+		  System.out.println("Calling swap function with inputs 2 & 3"); swap(-3,5);
+		  
+		  }
+		  
+		  private static void swap(int i, int j) { // TODO Auto-generated method stub
+		  
+		  System.out.println("values swap before: " + i +" + " +j); //swap logic 
+		  i = i + j; j = i - j; i = i - j;
+		  
+		  System.out.println("values swap after: " + i +" + " +j);
+		 
 	}
 
 }
